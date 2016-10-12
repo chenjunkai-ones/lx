9b7df2c00725f4a2b5b8fd2307db764255daaa09
chenjunkai <302656338@qq.com>
2016-10-12 21:38:51 +0800

    update gulpfile.jhs

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
-var gulp = require('gulp');
+"use strict";
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
 
-gulp.task('default', function () {
-	console.log('task start');
+
+gulp.task('sass', () => {
+    return gulp.src("scss/lx.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("dist/css"))
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest("dist/css"))
 });
+
+gulp.task('js', () => {
+    return gulp.src('js/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('default', ['sass', 'js']);
---
+++ js/alert.js
@@ -0,0 +1,28 @@
+/**
+ * ━━━━━━神兽出没━━━━━━
+ *     ┏┓   ┏┓
+ *    ┏┛┻━━━┛┻┓
+ *    ┃       ┃
+ *    ┃   ━   ┃
+ *    ┃ ┳┛ ┗┳ ┃
+ *    ┃       ┃
+ *    ┃   ┻   ┃
+ *    ┃       ┃
+ *    ┗━┓   ┏━┛Code is far away from bug with the animal protecting
+ *      ┃   ┃  神兽保佑,代码无bug
+ *      ┃   ┃  Created by cjk on 16/8/5. your graves are ready
+ *      ┃   ┗━━━┓
+ *      ┃       ┣┓
+ *      ┃       ┏┛
+ *      ┗┓┓┏━┳┓┏┛
+ *       ┃┫┫ ┃┫┫
+ *       ┗┻┛ ┗┻┛
+ *
+ * ━━━Great Beast Is Here━━━ */
+const message = 1;
+
+var logs = (msg) => {
+  if(console) console.log(msg);
+};
+
+log(message);
